e16c63ef13efe213f96b38fbe25fedd46a0ca9c2
Yulin Wang <yulin0629@gmail.com>
2019-03-15 04:36:59 +0800

    finish page select

--- js/all.js
+++ js/all.js
@@ -1,22 +1,35 @@
 // data
 var zones = new Set(data.result.records.map(x => x.Zone));
 zones = Array.from(zones);
+const SPOTS_SHOW_PER_ZONE = 5;
+let currentSpots = [];
+let currentPage = 0;
 // DOM
 var distinct_select = document.getElementById('distinct-select');
 var hot_distinct_list = document.getElementById('hot-district-list');
+var page_list = document.querySelector('.page-list');
+var page_prev = document.querySelector('.page-prev');
+var page_next = document.querySelector('.page-next');
 
 // Register Event
 distinct_select.addEventListener('change', onZoneChanged);
 hot_distinct_list.addEventListener('click', onHotDistinctListClick);
+page_list.addEventListener('click', onPageListClick);
+page_prev.addEventListener('click', onPagePrevClick);
+page_next.addEventListener('click', onPageNextClick);
 
 // View
 initialSelect();
 
 
 
-function onZoneChanged(e) {
-    let spots = getZoneSpotData(e.target.value);
-    updateSpots(spots, e.target.value);
+function onZoneChanged() {
+    currentPage = 0;
+    getZoneSpotData(e.target.value);
+    updateHeader(e.target.value);
+    let start = currentPage * SPOTS_SHOW_PER_ZONE;
+    updateSpots(start, SPOTS_SHOW_PER_ZONE);
+    updatePageList();
 }
 
 function onHotDistinctListClick(e) {
@@ -24,13 +37,48 @@ function onHotDistinctListClick(e) {
     if (e.target.nodeName !== 'LI') {
         return;
     }
-    let spots = getZoneSpotData(e.target.textContent);
-    updateSpots(spots, e.target.textContent);
+    currentPage = 0;
+    getZoneSpotData(e.target.textContent);
+    updateHeader(e.target.textContent);
+    let start = currentPage * SPOTS_SHOW_PER_ZONE;
+    updateSpots(start, SPOTS_SHOW_PER_ZONE);
+    updatePageList();
 }
 
-function getZoneSpotData(zone) {
+function onPageListClick(e) {
+    console.log(e.target.nodeName);
+    if (e.target.nodeName !== 'A') {
+        return;
+    }
+    e.preventDefault();
+    currentPage = parseInt(e.target.dataset.index);
+    let start = currentPage * SPOTS_SHOW_PER_ZONE;
+    updateSpots(start, start + SPOTS_SHOW_PER_ZONE);
+    updatePageList();
+}
+
+function onPagePrevClick(e) {
+    e.preventDefault();
+    currentPage -= 1;
+    console.log(currentPage);
+    let start = currentPage * SPOTS_SHOW_PER_ZONE;
+    updateSpots(start, start + SPOTS_SHOW_PER_ZONE);
+    updatePageList();
+}
+
+function onPageNextClick(e) {
+    e.preventDefault();
+    currentPage += 1;
+    let start = currentPage * SPOTS_SHOW_PER_ZONE;
+    updateSpots(start, start + SPOTS_SHOW_PER_ZONE);
+    updatePageList();
+}
+
+
+
+function getZoneSpotData(zone, start=0, end=10) {
     let spots = data.result.records.filter(x => x.Zone == zone);
-    return spots;
+    currentSpots = spots;
 }
 
 function initialSelect() {
@@ -44,13 +92,17 @@ function initialSelect() {
     }
 }
 
-function updateSpots(spots, zoneName) {
+function updateHeader(zoneName) {
     let header = document.querySelector('.content-header');
     header.textContent = zoneName;
+}
 
+function updateSpots(start=0, end=Infinity) {
+    spots = currentSpots
     let zone_list = document.querySelector('.zone-list');
     zone_list.innerHTML = '';
-    for (let i = 0; i < spots.length; i++) {
+
+    for (let i = start; i < spots.length && i < end; i++) {
         const zoneData = spots[i];
         zone_list.innerHTML += `<li>
         <div class="spot-img">
@@ -82,5 +134,28 @@ function updateSpots(spots, zoneName) {
         </ul>
         <div class="spot-tag"><img class="icon" src="images/icons_tag.png" alt="icon tag">${zoneData.Ticketinfo}</div>
         </li>`;
+    }
+}
+
+function updatePageList() {
+    let list = document.querySelector('.page-list');
+    list.innerHTML = '';
+    let length = Math.ceil(currentSpots.length / SPOTS_SHOW_PER_ZONE);
+    for (let i = 0; i < length; i++) {
+        let active = i === currentPage ? 'active' : '';
+        list.innerHTML += `<a class="${active}" data-index="${i}" href="">${i+1}</a>`
+    }
+
+    if (currentPage <= 0) {
+        page_prev.classList.add('disabled');
+        page_prev.href
+    } else {
+        page_prev.classList.remove('disabled');
+    }
+
+    if (currentPage >= length-1) {
+        page_next.classList.add('disabled');
+    } else {
+        page_next.classList.remove('disabled');
     }
 }
